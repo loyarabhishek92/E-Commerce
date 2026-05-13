30d23cc2bf4b7e3144cf6154255ef79ade4c70ab
loyarabhishek92 <loyarabhishek92@gmail.com>
2026-05-13 15:45:23 +0545

    completed

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -2,12 +2,13 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { createApi } from "@reduxjs/toolkit/query/react";
 
 
-export const base = 'http://192.168.18.24:5000';
+// export const base = 'http://192.168.18.24:5000';
+export const base = 'https://e-commerce-m4xn.onrender.com';
 
 export const mainApi = createApi({
     reducerPath: 'mainApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://192.168.18.24:5000/api'
+        baseUrl: 'https://e-commerce-m4xn.onrender.com/api'
     }),
     endpoints: (builder) => ({}),
 });
