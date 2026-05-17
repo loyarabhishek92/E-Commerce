31c3a88c99326ed74047fcc855248d3a2d0ccab3
loyarabhishek92 <loyarabhishek92@gmail.com>
2026-05-17 16:14:58 +0545

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
     <div className="px-5 mt-1.5 flex justify-between items-center sticky top-0 z-1 bg-gray-100 rounded-b-2xl flex-wrap">
 
       <div className="flex items-center">
-        <img src="../public/logobgremover.png" alt="logo" className="w-15 h-15" />
+        <img src="/logobgremover.png" alt="logo" className="w-15 h-15" />
         <NavLink to={'/'} className='font-extrabold font-serif text-orange-500'>Networking Hub</NavLink>
       </div>
 
